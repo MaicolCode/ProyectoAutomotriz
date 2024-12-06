cb4bfa15a317ca1a2b7ae5d2716c26b021ecb7a7
MaicolCode <paucarjhon07@outlook.com>
2024-12-06 15:01:39 -0500

    Refactorizacion de diseños de los posts

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -9,17 +9,17 @@ inline-flex items-center px-1 pt-1 hover:border-b-2 hover:border-rgba-blue-sky-2
         Posts
     </x-slot>
     
-    <section class="relative w-full lg:min-h-screen flex flex-col items-center">
+    <section class="relative w-full min-h-auto flex flex-col items-center">
         {{-- Seccion del Header --}}
-        <div class=" relative shadow-md flex items-center" style="height: 700px">
+        <div class="relative shadow-md flex items-center mt-40 sm:mt-10" style="height: 700px">
             <img src="https://www.nicepng.com/png/full/120-1207720_mecanico-de-cuerpo-entero.png"
-                alt="mecanico-de-cuerpo-entero_By_NicePng" class="absolute bottom-0  right-0 z-20 mr-10 w-44 lg:w-60">
+                alt="mecanico-de-cuerpo-entero_By_NicePng" class="absolute bottom-0  right-[-20px] z-20 mr-10 w-32 lg:w-60">
             <article class="skewed bg-rgba-blue-sky-100">
             </article>
-            <div class="px-16 z-10">
-                <b class=" text-4xl">Sección de recomendaciones</b>
+            <div class="px-10 sm:px-16 z-10">
+                <b class="text-4xl text-white">Sección de recomendaciones</b>
                 <hr>
-                <p class="w-3/4 lg:w-3/5 mt-10 text-xl lg:text-lg">Automotriz Tipan a parte de ofrecerte sus servicios,
+                <p class="w-3/2 lg:w-3/5 mt-10 text-xl lg:text-lg font-medium text-slate-900 p-4 bg-gray-400 rounded-md bg-opacity-25">Automotriz Tipan a parte de ofrecerte sus servicios,
                     te ofrece contenido basado
                     en recomendaciones que contienen información relevante, la cual te permitira
                     la posibilidad de que tu mismo puedas mantener de una mejor forma tu vehiculo sin la necesidad
@@ -37,17 +37,17 @@ inline-flex items-center px-1 pt-1 hover:border-b-2 hover:border-rgba-blue-sky-2
             </div>
         </div>
         {{-- Seccion de las habilidades --}}
-        <div class="my-10 relative shadow-md flex flex-col items-center w-full lg:w-3/6 z-10 bg-blue-50 rounded-lg">
-            <b class="text-4xl mt-10 text-center">Dentro de esta seccion encontraras: </b>
-            <div class="timeList mt-5 px-20 py-10">
+        <div class="my-10 relative shadow-md flex flex-col items-center w-[90%] lg:w-3/6 z-10 bg-blue-50 rounded-lg">
+            <b class="text-3xl lg:text-4xl mt-10 text-center">Dentro de esta sección encontraras: </b>
+            <div class="timeList mt-5 px-14 py-8 lg:px-20 lg:py-10">
                 <ol class="relative border-s border-gray-200 dark:border-gray-700">
                     <li class="mb-10 ms-4">
                         <div
                             class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                         </div>
                         <ion-icon class="w-12 h-12 lg:w-6 lg:h-6" name="checkmark-done-circle-outline"></ion-icon>
-                        <h3 class="text-3xl lg:text-lg font-semibold text-rgba-blue-sky-100">Recomendaciones</h3>
-                        <p class="text-xl lg:text-sm font-normal text-gray-500 dark:text-gray-400">
+                        <h3 class="text-xl lg:text-lg font-semibold text-rgba-blue-sky-100">Recomendaciones</h3>
+                        <p class="text-md lg:text-sm font-normal text-gray-500 dark:text-gray-400">
                             Como podras ver la mayoria de las personas acuden a un profesional, debido a que su vehiculo
                             a presentado fallas,
                             esto se debe al mal estado en el que mantenemos a nuestro vehiculo, por ende te presentamos
@@ -61,8 +61,8 @@ inline-flex items-center px-1 pt-1 hover:border-b-2 hover:border-rgba-blue-sky-2
                             class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                         </div>
                         <ion-icon class="w-12 h-12 lg:w-6 lg:h-6" name="footsteps-outline"></ion-icon>
-                        <h3 class="text-3xl lg:text-lg font-semibold text-rgba-blue-sky-100">Instrucciones</h3>
-                        <p class=" text-xl lg:text-sm  font-normal text-gray-500 dark:text-gray-400">
+                        <h3 class="text-xl lg:text-lg font-semibold text-rgba-blue-sky-100">Instrucciones</h3>
+                        <p class=" text-md lg:text-sm  font-normal text-gray-500 dark:text-gray-400">
                             El hacer mantenimiento sin haber tenido alguna experiencia, podria ocasionar algun tipo de
                             conflicto en el proceso, por ende en
                             esta seccion te afrecemos algunas instrucciones que deberas de seguir, para realizar el
@@ -74,8 +74,8 @@ inline-flex items-center px-1 pt-1 hover:border-b-2 hover:border-rgba-blue-sky-2
                             class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                         </div>
                         <ion-icon class="w-12 h-12 lg:w-6 lg:h-6" name="construct-outline"></ion-icon>
-                        <h3 class="text-3xl lg:text-lg font-semibold text-rgba-blue-sky-100">Herramientas </h3>
-                        <p class=" text-xl lg:text-sm  font-normal text-gray-500 dark:text-gray-400">
+                        <h3 class="text-xl lg:text-lg font-semibold text-rgba-blue-sky-100">Herramientas </h3>
+                        <p class=" text-md lg:text-sm  font-normal text-gray-500 dark:text-gray-400">
                             El hacer uso de las herramientas adecuadas, al realizar algun mantenimiento
                             nos ayudaran a prevenir daños en los componentes de nuestro vehiculo, aqui te ofrecemos
                             información acerca de que herramientas
@@ -88,8 +88,8 @@ inline-flex items-center px-1 pt-1 hover:border-b-2 hover:border-rgba-blue-sky-2
                             class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                         </div>
                         <ion-icon class="w-12 h-12 lg:w-6 lg:h-6" name="hand-right-outline"></ion-icon>
-                        <h3 class="text-3xl lg:text-lg font-semibold text-rgba-blue-sky-100">Precauciones</h3>
-                        <p class=" text-xl lg:text-sm  font-normal text-gray-500 dark:text-gray-400">
+                        <h3 class="text-xl lg:text-lg font-semibold text-rgba-blue-sky-100">Precauciones</h3>
+                        <p class=" text-md lg:text-sm  font-normal text-gray-500 dark:text-gray-400">
                             Al realizar un mantenimiento, hay que tener en cuenta las precauciones que se deben de
                             tomar a la hora de que nos ponemos a manipular las piezas de nuestro vehiculo, estas
                             evitaran que dañemos
@@ -111,24 +111,24 @@ inline-flex items-center px-1 pt-1 hover:border-b-2 hover:border-rgba-blue-sky-2
             </div>
             {{-- Seccion del carrusel de Posts --}}
             <div class="flex justify-center">
-                <div id="indicators-carousel" class="relative w-3/5 z-0" data-carousel="static">
+                <div id="indicators-carousel" class="relative w-[90%] sm:w-3/5 z-0" data-carousel="static">
                     <!-- Carousel wrapper -->
-                    <div class="flex w-full relative overflow-hidden rounded-lg md:h-96" style="height: 500px">
+                    <div class="flex w-full relative overflow-hidden rounded-md shadow-[0_4px_10px_1px_rgba(0,0,0,0.4)] md:h-96" style="height: 500px">
                         <!-- Item 1 -->
-                        <div class="flex duration-1000 ease-in-out  justify-center w-full" style="height: 450px"
+                        <div class="flex duration-1000 ease-in-out  justify-center w-full h-full" 
                             data-carousel-item="active">
                             <div
-                                class="relative flex items-center justify-center p-6 bg-white rounded-lg shadow-xl shadow-slate-800 dark:bg-gray-400 dark:border-gray-700 dark:bg-opacity-40" style="width:500px">
-                                <h2 class="text-2xl text-white text-center">Aquí verás todo el contenido compartido por la empresa.</h2>
+                                class="relative flex py-40 sm:items-center justify-center p-6 bg-white rounded-lg dark:bg-gray-400 dark:border-gray-700 dark:bg-opacity-40 w-full">
+                                <h2 class="text-xl sm:text-2xl text-white text-center">Aquí verás todo el contenido compartido por la empresa.</h2>
 
                             </div>
                         </div>
 
                         @foreach ($posts as $post)
-                            <div class="flex duration-1000 ease-in-out  justify-center" style="height: 450px"
+                            <div class="flex duration-1000 ease-in-out  justify-center h-full"
                                 data-carousel-item>
                                 <div
-                                    class="relative  max-w-4xl bg-white rounded-lg shadow-xl shadow-slate-800 dark:bg-gray-400  dark:border-gray-700 dark:bg-opacity-50" style="width:500px">
+                                    class="relative  max-w-4xl bg-white rounded-lg dark:bg-gray-400  dark:border-gray-700 dark:bg-opacity-50 w-full">
                                     <img class="absolute top-0 left-0 object-cover rounded-t-lg  w-full h-2/4 -z-0"
                                         src="{{ $post->image }}" alt="" />
                                     <div class="relative flex w-2/4 h-2/4 items-center">
